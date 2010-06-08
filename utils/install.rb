5d5903805c640fa7ae936b922e9265330d01bbf7
Chris Brown <cb@tardis.ed.ac.uk>
2010-06-08 19:58:27 +0100

    New updater system

--- utils/install.rb
+++ utils/install.rb
@@ -12,17 +12,28 @@ def command(command, dir = '.')
 end
 
 AT_TOOLS_DIR = File.expand_path('~/.at-tools/')
+AT_TOOLS_BIN = AT_TOOLS_DIR + '/bin'
 
 puts "Making directories"
-FileUtils.mkdir(AT_TOOLS_DIR + '/bin')
+FileUtils.mkdir(AT_TOOLS_BIN)
 
 puts "Linking update scripts"
 FileUtils.ln_s(AT_TOOLS_DIR + '/utils/update.rb',
-               AT_TOOLS_DIR + '/bin/at-tools-update')
+               AT_TOOLS_BIN + '/at-tools-update')
+
+puts "Linking programs"
+FileUtils.ln_s(AT_TOOLS_DIR + '/cascader/bin/cascader',
+               AT_TOOLS_BIN + '/cascader')
+
+FileUtils.ln_s(AT_TOOLS_DIR + '/print/bin/print',
+               AT_TOOLS_BIN + '/print')
+
+FileUtils.ln_s(AT_TOOLS_DIR + '/cen/cen.rb',
+               AT_TOOLS_BIN + '/cen')
 
 puts "Updating at-tools"
 command('./update.rb', AT_TOOLS_DIR + '/utils')
 
 puts "Updating PATH"
-command('echo "PATH=$PATH:"' + AT_TOOLS_DIR + '/bin' + ' >> ~/.bash_profile')
-command('source .bash_profile', File.expand_path('~'))
+command('echo "PATH=$PATH:' + AT_TOOLS_BIN + '" >> ~/.bash_profile')
+command('source ~/.bash_profile')
